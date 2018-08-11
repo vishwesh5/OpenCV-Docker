e930ecb3fabac816d86ff8b358dbb8bd171f16ea
Ubuntu <ubuntu@ip-172-31-21-18.us-west-2.compute.internal>
2018-08-11 05:36:12 +0000

    Update Launch Cluster script

--- LaunchCluster.sh
+++ LaunchCluster.sh
@@ -20,7 +20,7 @@ curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
 kubectl --namespace kube-system create serviceaccount tiller
 kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
 helm init --service-account tiller
-helm version
+#helm version
 kubectl --namespace=kube-system patch deployment tiller-deploy --type=json --patch='[{"op": "add", "path": "/spec/template/spec/containers/0/command", "value": ["/tiller", "--listen=localhost:44134"]}]'
 
 helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
