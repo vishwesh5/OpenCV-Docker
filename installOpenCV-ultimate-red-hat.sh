aef1271e0511ee4e2c387eeba284ae9e7e4a69cc
vishwesh5 <vishweshshrimali5@gmail.com>
2018-12-03 17:22:02 +0530

    Update installOpenCV-ultimate-red-hat.sh

--- installOpenCV-ultimate-red-hat.sh
+++ installOpenCV-ultimate-red-hat.sh
@@ -15,35 +15,37 @@ rm -rf opencv_contrib/build
 cwd=$(pwd)
 sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo yum -y install epel-release
-sudo yum -y install git cmake gcc-c++ cmake3
+sudo yum -y install git gcc-c++ cmake3
 sudo yum -y install qt5-qtbase-devel
-#sudo yum -y install https://centos7.iuscommunity.org/ius-release.rpm
-sudo yum install -y python36 python36-devel python36-setuptools
-sudo easy_install-3.6 pip
+sudo yum install -y python34 python34-devel python34-pip
+sudo yum install -y python python-devel python-pip
 
-#sudo yum -y install python37
-#sudo yum -y install python3-devel
-#sudo yum -y install git cmake gcc-c++
-sudo yum -y install python-devel numpy
+sudo yum -y install python-devel numpy python34-numpy
 sudo yum -y install gtk2-devel
-#sudo yum -y install libdc1394
-#sudo yum -y install libdc1394-devel
 
+sudo rpm --import http://li.nux.ro/download/nux/RPM-GPG-KEY-nux.ro
+sudo rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm
+sudo yum install -y ffmpeg #2.6.8
+sudo yum install -y ffmpeg-devel
+
+sudo yum install -y libpng-devel
+#sudo yum install -y jasper-devel
+sudo yum install -y openexr-devel
+sudo yum install -y libwebp-devel
 sudo yum -y install libjpeg-turbo-devel 
 sudo yum install -y freeglut-devel mesa-libGL mesa-libGL-devel
 sudo yum -y install libtiff-devel 
 sudo yum -y install libdc1394-devel --skip-broken
 sudo yum -y install tbb-devel eigen3-devel
-#curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
-#sudo python get-pip.py
+sudo yum -y install boost boost-thread boost-devel
 
-#sudo pip install virtualenv virtualenvwrapper
+sudo pip3 install virtualenv virtualenvwrapper
 #pip3 install virtualenv virtualenvwrapper
-#echo "export WORKON_HOME=$HOME/.virtualenvs" >> ~/.bashrc
-#echo "source /usr/bin/virtualenvwrapper.sh" >> ~/.bashrc
-#echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3.6" >> ~/.bashrc
-#export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3.6
-#source /usr/bin/virtualenvwrapper.sh
+echo "export WORKON_HOME=$HOME/.virtualenvs" >> ~/.bashrc
+echo "source /usr/bin/virtualenvwrapper.sh" >> ~/.bashrc
+echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3" >> ~/.bashrc
+export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
+source /usr/bin/virtualenvwrapper.sh
 
 #sudo yum -y install libv4l-devel
 #sudo yum -y install ffmpeg-devel
@@ -52,16 +54,18 @@ sudo yum -y install tbb-devel eigen3-devel
 cd $cwd
 ############ For Python 3 ############
 # create virtual environment
-python3.6 -m venv OpenCV-"$cvVersion"-py3
-echo "# Virtual Environment Wrapper" >> ~/.bashrc
-echo "alias workoncv-$cvVersion=\"source $cwd/OpenCV-$cvVersion-py3/bin/activate\"" >> ~/.bashrc
-source "$cwd"/OpenCV-"$cvVersion"-py3/bin/activate
-
-#workon OpenCV-"$cvVersion"-py3
-# now install python libraries within this virtual environment
-pip install wheel
-pip install numpy scipy matplotlib scikit-image scikit-learn ipython dlib
- 
+#python3.6 -m venv OpenCV-"$cvVersion"-py3
+#echo "# Virtual Environment Wrapper" >> ~/.bashrc
+#echo "alias workoncv-$cvVersion=\"source $cwd/OpenCV-$cvVersion-py3/bin/activate\"" >> ~/.bashrc
+#source "$cwd"/OpenCV-"$cvVersion"-py3/bin/activate
+mkvirtualenv OpenCV-"$cvVersion"-py3 -p python3
+workon OpenCV-"$cvVersion"-py3
+pip install cmake numpy scipy matplotlib scikit-image scikit-learn ipython dlib
+# quit virtual environment
+deactivate
+mkvirtualenv OpenCV-"$cvVersion"-py2 -p python2
+workon OpenCV-"$cvVersion"-py2
+pip install cmake numpy scipy matplotlib scikit-image scikit-learn ipython dlib
 # quit virtual environment
 deactivate
 ######################################
@@ -70,7 +74,7 @@ git clone https://github.com/opencv/opencv.git
 cd opencv
 git checkout tags/3.4.3
 cd ..
- 
+
 git clone https://github.com/opencv/opencv_contrib.git
 cd opencv_contrib
 git checkout tags/3.4.3
@@ -84,6 +88,9 @@ sed -i '38s/.*/  ocv_target_link_libraries(${tgt} ${OPENCV_LINKER_LIBS} ${OPENCV
 mkdir build
 cd build
 
+            -D OPENCV_PYTHON3_INSTALL_PATH=$HOME/.virtualenvs/OpenCV-$cvVersion-py3/lib/python3.6/site-packages \
+            -D OPENCV_PYTHON2_INSTALL_PATH=$HOME/.virtualenvs/OpenCV-$cvVersion-py2/lib/python2.7/site-packages \
+            
 cmake3 -D CMAKE_BUILD_TYPE=RELEASE \
             -D CMAKE_INSTALL_PREFIX=/usr/local \
             -D INSTALL_C_EXAMPLES=ON \
@@ -92,9 +99,9 @@ cmake3 -D CMAKE_BUILD_TYPE=RELEASE \
             -D WITH_V4L=ON \
             -D OPENCV_SKIP_PYTHON_LOADER=ON \
             -D OPENCV_GENERATE_PKGCONFIG=ON \
-            -D OPENCV_PYTHON3_INSTALL_PATH=$cwd/OpenCV-$cvVersion-py3/lib/python3.6/site-packages \
         -D WITH_QT=ON \
         -D WITH_OPENGL=ON \
+        -D PYTHON_DEFAULT_EXECUTABLE=/usr/bin/python3 \
         -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
         -D BUILD_EXAMPLES=ON ..
         
