f4333c732e6ce255c8313b0b20da5fdea8228f2d
vishwesh5 <vishweshshrimali5@gmail.com>
2018-12-04 08:25:08 +0530

    Update installOpenCV-ultimate-red-hat.sh

--- installOpenCV-ultimate-red-hat.sh
+++ installOpenCV-ultimate-red-hat.sh
@@ -156,9 +156,9 @@ git checkout 3.4
 cd ..
 
 cd opencv
-echo "find_package(OpenGL REQUIRED)" >>./samples/cpp/CMakeLists.txt
-echo "find_package(GLUT REQUIRED)" >> ./samples/cpp/CMakeLists.txt
-sed -i '38s/.*/  ocv_target_link_libraries(${tgt} ${OPENCV_LINKER_LIBS} ${OPENCV_CPP_SAMPLES_REQUIRED_DEPS} ${OPENGL_LIBRARIES} ${GLUT_LIBRARY})/' ./samples/cpp/CMakeLists.txt
+#echo "find_package(OpenGL REQUIRED)" >>./samples/cpp/CMakeLists.txt
+#echo "find_package(GLUT REQUIRED)" >> ./samples/cpp/CMakeLists.txt
+#sed -i '38s/.*/  ocv_target_link_libraries(${tgt} ${OPENCV_LINKER_LIBS} ${OPENCV_CPP_SAMPLES_REQUIRED_DEPS} ${OPENGL_LIBRARIES} ${GLUT_LIBRARY})/' ./samples/cpp/CMakeLists.txt
 mkdir build
 cd build
 
