239f709695b3b40b0c14bbc575ae29fd232b4dfb
vishwesh5 <vishweshshrimali5@gmail.com>
2018-12-20 14:40:04 +0530

    Create installOpenCV-ultimate-18-04-python-3-7.sh

---
+++ installOpenCV-ultimate-18-04-python-3-7.sh
@@ -0,0 +1,235 @@
+#!/bin/sh
+
+echo "OpenCV installation by learnOpenCV.com"
+
+echo "Installing OpenCV - 3.4.3"
+ 
+#Specify OpenCV version
+cvVersion="3.4.3"
+
+# Clean build directories
+rm -rf opencv/build
+rm -rf opencv_contrib/build
+
+# Save current working directory
+cwd=$(pwd)
+sudo apt -y update
+sudo apt -y upgrade
+
+sudo apt -y remove x264 libx264-dev
+ 
+## Install dependencies
+sudo apt -y install build-essential checkinstall cmake pkg-config yasm
+sudo apt -y install git gfortran
+sudo apt -y install libjpeg8-dev libpng-dev
+ 
+sudo apt -y install software-properties-common
+sudo add-apt-repository "deb http://security.ubuntu.com/ubuntu xenial-security main"
+sudo apt -y update
+ 
+sudo apt -y install libjasper1
+sudo apt -y install libtiff-dev
+ 
+sudo apt -y install libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev
+sudo apt -y install libxine2-dev libv4l-dev
+cd /usr/include/linux
+sudo ln -s -f ../libv4l1-videodev.h videodev.h
+cd "$cwd"
+ 
+sudo apt -y install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
+sudo apt -y install libgtk2.0-dev libtbb-dev qt5-default
+sudo apt -y install libatlas-base-dev
+sudo apt -y install libfaac-dev libmp3lame-dev libtheora-dev
+sudo apt -y install libvorbis-dev libxvidcore-dev
+sudo apt -y install libopencore-amrnb-dev libopencore-amrwb-dev
+sudo apt -y install libavresample-dev
+sudo apt -y install x264 v4l-utils
+ 
+# Optional dependencies
+sudo apt -y install libprotobuf-dev protobuf-compiler
+sudo apt -y install libgoogle-glog-dev libgflags-dev
+sudo apt -y install libgphoto2-dev libeigen3-dev libhdf5-dev doxygen
+
+sudo apt -y install python3-dev python3-pip python3-venv
+sudo -H pip3 install -U pip numpy
+sudo apt -y install python3-testresources
+
+# Install virtual environment
+
+cd $cwd
+############ For Python 3 ############
+# create virtual environment
+python3 -m venv OpenCV-"$cvVersion"-py3
+echo "# Virtual Environment Wrapper" >> ~/.bashrc
+echo "alias workoncv-$cvVersion=\"source $cwd/OpenCV-$cvVersion-py3/bin/activate\"" >> ~/.bashrc
+source "$cwd"/OpenCV-"$cvVersion"-py3/bin/activate
+
+# now install python libraries within this virtual environment
+pip install wheel
+pip install numpy scipy matplotlib scikit-image scikit-learn ipython dlib
+ 
+# quit virtual environment
+deactivate
+######################################
+
+git clone https://github.com/opencv/opencv.git
+cd opencv
+git checkout tags/3.4.3
+cd ..
+ 
+git clone https://github.com/opencv/opencv_contrib.git
+cd opencv_contrib
+git checkout tags/3.4.3
+cd ..
+
+cd opencv
+mkdir build
+cd build
+
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+            -D CMAKE_INSTALL_PREFIX=/usr/local \
+            -D INSTALL_C_EXAMPLES=ON \
+            -D INSTALL_PYTHON_EXAMPLES=ON \
+            -D WITH_TBB=ON \
+            -D WITH_V4L=ON \
+            -D OPENCV_SKIP_PYTHON_LOADER=ON \
+            -D OPENCV_PYTHON3_INSTALL_PATH=$cwd/OpenCV-$cvVersion-py3/lib/python3.6/site-packages \
+        -D WITH_QT=ON \
+        -D WITH_OPENGL=ON \
+        -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
+        -D BUILD_EXAMPLES=ON ..
+        
+make -j$(nproc)
+sudo make install
+sudo sh -c 'echo "/usr/local/lib" >> /etc/ld.so.conf.d/opencv.conf'
+sudo ldconfig
+cd $cwd
+
+#=========================================================================
+
+echo "Installing OpenCV - 3.4.4"
+ 
+#Specify OpenCV version
+cvVersion="3.4.4"
+
+# Clean build directories
+rm -rf opencv/build
+rm -rf opencv_contrib/build
+
+# Create directory for installation
+mkdir installation
+mkdir installation/OpenCV-"$cvVersion"
+
+############ For Python 3 ############
+# create virtual environment
+cd $cwd
+python3 -m venv OpenCV-"$cvVersion"-py3
+echo "# Virtual Environment Wrapper" >> ~/.bashrc
+echo "alias workoncv-$cvVersion=\"source $cwd/OpenCV-$cvVersion-py3/bin/activate\"" >> ~/.bashrc
+source "$cwd"/OpenCV-"$cvVersion"-py3/bin/activate
+
+# now install python libraries within this virtual environment
+pip install wheel
+pip install numpy scipy matplotlib scikit-image scikit-learn ipython dlib
+ 
+# quit virtual environment
+deactivate
+######################################
+
+git clone https://github.com/opencv/opencv.git
+cd opencv
+git checkout 3.4
+cd ..
+ 
+git clone https://github.com/opencv/opencv_contrib.git
+cd opencv_contrib
+git checkout 3.4
+cd ..
+
+cd opencv
+mkdir build
+cd build
+
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+            -D CMAKE_INSTALL_PREFIX=$cwd/installation/OpenCV-"$cvVersion" \
+            -D INSTALL_C_EXAMPLES=ON \
+            -D INSTALL_PYTHON_EXAMPLES=ON \
+            -D WITH_TBB=ON \
+            -D WITH_V4L=ON \
+            -D OPENCV_SKIP_PYTHON_LOADER=ON \
+            -D OPENCV_PYTHON3_INSTALL_PATH=$cwd/OpenCV-$cvVersion-py3/lib/python3.6/site-packages \
+        -D WITH_QT=ON \
+        -D WITH_OPENGL=ON \
+        -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
+        -D BUILD_EXAMPLES=ON ..
+        
+make -j$(nproc)
+make install
+
+cd $cwd
+
+#=========================================================================
+
+echo "Installing OpenCV - 4.0.0"
+ 
+#Specify OpenCV version
+cvVersion="master"
+
+# Clean build directories
+rm -rf opencv/build
+rm -rf opencv_contrib/build
+
+# Create directory for installation
+mkdir installation
+mkdir installation/OpenCV-"$cvVersion"
+
+############ For Python 3 ############
+# create virtual environment
+cd $cwd
+python3 -m venv OpenCV-"$cvVersion"-py3
+echo "# Virtual Environment Wrapper" >> ~/.bashrc
+echo "alias workoncv-$cvVersion=\"source $cwd/OpenCV-$cvVersion-py3/bin/activate\"" >> ~/.bashrc
+source "$cwd"/OpenCV-"$cvVersion"-py3/bin/activate
+
+# now install python libraries within this virtual environment
+pip install wheel
+pip install numpy scipy matplotlib scikit-image scikit-learn ipython dlib
+ 
+# quit virtual environment
+deactivate
+######################################
+
+git clone https://github.com/opencv/opencv.git
+cd opencv
+git checkout master
+cd ..
+ 
+git clone https://github.com/opencv/opencv_contrib.git
+cd opencv_contrib
+git checkout master
+cd ..
+
+cd opencv
+mkdir build
+cd build
+
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+            -D CMAKE_INSTALL_PREFIX=$cwd/installation/OpenCV-"$cvVersion" \
+            -D INSTALL_C_EXAMPLES=ON \
+            -D INSTALL_PYTHON_EXAMPLES=ON \
+            -D WITH_TBB=ON \
+            -D WITH_V4L=ON \
+            -D OPENCV_SKIP_PYTHON_LOADER=ON \
+            -D OPENCV_PYTHON3_INSTALL_PATH=$cwd/OpenCV-$cvVersion-py3/lib/python3.6/site-packages \
+        -D WITH_QT=ON \
+        -D WITH_OPENGL=ON \
+        -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
+        -D BUILD_EXAMPLES=ON ..
+        
+make -j$(nproc)
+make install
+
+cd $cwd
+
+rm -rf opencv
+rm -rf opencv_contrib
